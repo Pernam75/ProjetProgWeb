2745befb6a8118e356d531901e1b67b8861dc169
Jules <jules.rubin@efrei.net>
2021-05-17 19:16:55 +0200

    add open in anew page link

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-/*Animation hamburgerMenu
-
-const hamburgerBtn = document.querySelectorAll('.hamburgerMenu');
-hamburgerBtn.forEach(btn => {
-    btn.addEventListener('click',() => {
-        btn.classList.toggle('active');
-    })
-});
-*/
-
+console.log("Salut");
